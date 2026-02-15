23cf1737798b8b7301d2caa6c789249395e44735
Konrad Ullrich <konrad.ullrich@moysies.de>
2026-02-15 21:26:22 +0100

    Update ESLint config to ignore additional directories for cleaner linting

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'docs'] },
+  { ignores: ['dist', 'docs', 'playwright/.cache', 'playwright-report', 'test-results'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
