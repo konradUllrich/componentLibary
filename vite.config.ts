1635c0b8ebc09f4a11c2a41c36bfdd40f5c4537b
Konrad Ullrich <konrad.ullrich@moysies.de>
2026-02-22 21:36:28 +0100

    style: format code in vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'index.ts'),
-      name: 'mpComponents',
-      formats: ['es', 'umd'],
-      fileName: (format) => `index.${format === 'es' ? 'js' : 'umd.js'}`
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "mpComponents",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format === "es" ? "js" : "umd.js"}`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
-  }
-})
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
