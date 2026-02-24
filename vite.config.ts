15f22ee824ba7677eeb8a1b5731972d76136700f
Konrad Ullrich <konrad.ullrich@moysies.de>
2026-02-24 21:20:15 +0100

    no 12

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ["index.ts", "common", "controls", "data-display", "layout"],
+      exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.tsx", "demo", "e2e", "playwright"],
+      tsconfigPath: "./tsconfig.json",
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "index.ts"),
